a67abd871f26147340d130dce253d5e5f02c9151
NP <nadezdapratashchyk@gmail.com>
2025-07-03 16:52:46 +0200

    modifeded testingRout

--- src/crud/testing-rout.ts
+++ src/crud/testing-rout.ts
@@ -4,10 +4,15 @@ import {blogsCollection, postsCollection} from "../db/mongo-db";
 export const testingRout = Router();
 
 testingRout.delete("/testing/all-data", async (req, res) => {
-    await Promise.all([
-            blogsCollection.deleteMany(),
-            postsCollection.deleteMany(),
-        ]
-    )
-    res.status(204);
+    try {
+        await Promise.all([
+            blogsCollection.deleteMany({}),
+            postsCollection.deleteMany({}),
+        ])
+        res.status(204).send();
+        console.log("Successfully deleted data");
+    } catch (err) {
+        res.status(500).send(err);
+        console.log("Error deleting data:", err);
+    }
 });
